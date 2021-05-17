6150a26dd9819743bdfd5d484983e638d505d30a
Andrea Pérez <andreaa.perz@gmail.com>
2021-05-16 21:25:43 -0500

    Primera parte

---
+++ scripts/autorizacion.js
@@ -0,0 +1,42 @@
+const formaingresar = document.getElementById("formaingresar");
+formaingresar.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    let correo = formaingresar['correo'].value;
+    let contrasena = formaingresar['contrasena'].value;
+
+    auth.signInWithEmailAndPassword(correo, contrasena).then(cred =>{
+        console.log("Entro");
+        $('#ingresarmodal').modal('hide');
+        formaingresar.reset();
+        formaingresar.querySelector('.error').innerHTML='';
+    }).catch(err =>{
+        console.log(err);
+        formaingresar.querySelector('.error').innerHTML=messageError(err.code);
+    });
+});
+
+function messageError(codigo){
+    let message = '';
+    switch(codigo){
+        case 'auth/wrong-password':
+            message = "Contraseña Incorrecta";
+            break;
+        case 'auth/user-not-found':
+            message = "Usuario no encontrado";
+            break;
+        case 'auth/weak-password':
+            message = "Contraeña débil";
+            break;
+        default:
+            message = "Hubo un error";
+    }
+    return message;
+}
+
+const salir = document.getElementById("salir");
+salir.addEventListener('click', (e) =>{
+    e.preventDefault();
+    auth.signOut().then( ()=>{
+        alert('Se ha cerrado sesión');
+    });
+});
