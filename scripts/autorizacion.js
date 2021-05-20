44f1c0ffb8fb11e8be0446c721347604696db974
Andrea Pérez <andreaa.perz@gmail.com>
2021-05-19 20:56:54 -0500

    Entrega 2

--- scripts/autorizacion.js
+++ scripts/autorizacion.js
@@ -1,23 +1,39 @@
+auth.onAuthStateChanged(user => {
+    console.log(user);
+    if (user) {
+        db.collection('platillos').onSnapshot( snapshot =>{
+            obtienePlatillos(snapshot.docs);
+        });
+
+        configurarMenu(user);
+
+    } else {
+        obtienePlatillos([]);
+        configurarMenu();
+    }
+});
+
 const formaingresar = document.getElementById("formaingresar");
-formaingresar.addEventListener('submit', (e) =>{
+
+formaingresar.addEventListener('submit', (e) => {
     e.preventDefault();
     let correo = formaingresar['correo'].value;
     let contrasena = formaingresar['contrasena'].value;
 
-    auth.signInWithEmailAndPassword(correo, contrasena).then(cred =>{
+    auth.signInWithEmailAndPassword(correo, contrasena).then(cred => {
         console.log("Entro");
         $('#ingresarmodal').modal('hide');
         formaingresar.reset();
-        formaingresar.querySelector('.error').innerHTML='';
-    }).catch(err =>{
+        formaingresar.querySelector('.error').innerHTML = '';
+    }).catch(err => {
         console.log(err);
-        formaingresar.querySelector('.error').innerHTML=messageError(err.code);
+        formaingresar.querySelector('.error').innerHTML = messageError(err.code);
     });
 });
 
-function messageError(codigo){
+function messageError(codigo) {
     let message = '';
-    switch(codigo){
+    switch (codigo) {
         case 'auth/wrong-password':
             message = "Contraseña Incorrecta";
             break;
@@ -34,9 +50,60 @@ function messageError(codigo){
 }
 
 const salir = document.getElementById("salir");
-salir.addEventListener('click', (e) =>{
+salir.addEventListener('click', (e) => {
     e.preventDefault();
-    auth.signOut().then( ()=>{
+    auth.signOut().then(() => {
         alert('Se ha cerrado sesión');
     });
 });
+
+const formaregistrate = document.getElementById('formaregistrar');
+
+formaregistrate.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const correo = formaregistrate['rcorreo'].value;
+    const contrasena = formaregistrate['rcontrasena'].value;
+
+    auth.createUserWithEmailAndPassword(correo, contrasena).then(cred => {
+        return db.collection('usuarios').doc(cred.user.uid).set({
+            nombre: formaregistrate['rnombre'].value,
+            telefono: formaregistrate['rtelefono'].value,
+            direccion: formaregistrate['rdireccion'].value
+        });
+    }).then(() => {
+        $('#registrarmodal').modal('hide');
+        formaregistrate.reset();
+        formaregistrate.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        formaregistrate.querySelector('.error').innerHTML = messageError(err.code);
+    });
+});
+
+entrarGoogle = () =>{
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result){
+        var token = result.credential.accessToken;
+
+        console.log(token);
+
+        var user = result.user;
+
+        let html = `
+        
+        <p> Nombre: ${user.displayName} </p>
+        <p> Email: ${user.email} </p>
+        <img src="${user.photoURL}">
+        `;
+
+        datosdelacuenta.innerHTML = html;
+
+        $('#ingresarmodal').modal('hide');
+        formaingresar.reset();
+        formaingresar.querySelector('.error').innerHTML = '';
+
+        }).catch(function(error){
+            console.log(error);
+        })
+}
